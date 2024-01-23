9e3f31a91eab3cb6784d5c24e0162cead963a50a
nikul makvana <nikulmakvana767@gmail.com>
2024-01-23 18:04:03 +0530

    Added tailwindcss

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>hellow nik</div>;
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello nik!</h1>
+    </div>
+  );
 };
 
 export default Home;
